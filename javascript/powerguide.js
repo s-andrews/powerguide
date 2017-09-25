1bdd85fb985f55314e92696ab1e0a2cacb296411
Simon Andrews <simon.andrews@babraham.ac.uk>
2017-09-25 14:06:49 +0100

    Basic question switching works

--- javascript/powerguide.js
+++ javascript/powerguide.js
@@ -1,20 +1,42 @@
+var questions = {
+				type: {
+					question_text: 'What type of data do you have?',
+					question_explanation: 'Data comes in different types',
+					answers: [
+						{ text: 'Continuous' },
+						{ text: 'Categorical' }
+					]
+				},
+
+				normal: {
+					question_text: 'Is your data normal?',
+					question_explanation: 'Relies on a mean and stdev being meaningful',
+					answers: [
+						{ text: 'Yes' },
+						{ text: 'No' }
+					]
+				},
+		}
+
+var question = 'type'
+
 var app = new Vue({
 	el: '#app',
+
 	data: {
-		question_text: 'What is your name',
-		question_explanation: 'We need to know what to call you for the rest of the day',
-		answers: [
-			{ text: 'Option 1' },
-			{ text: 'Option 2' },
-			{ text: 'Option 3' },
-			{ text: 'Option 4' },
-		],
+		question_text: questions[question].question_text,
+		question_explanation: questions[question].question_explanation,
+		answers: questions[question].answers,
 		selected_answer: 'no answer'
 	},
 
 	methods: {
 		next_question: function () {
-			this.question_text = 'What is your quest'
+			this.question = 'normal';
+			this.question_text = questions[this.question].question_text;
+			this.question_explanation = questions[this.question].question_explanation;
+			this.answers = questions[this.question].answers;
+			this.selected_answer = 'no answer'
 		}
 	}
 })
